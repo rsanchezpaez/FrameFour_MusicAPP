8a323a6e30fd99ae177113641ae4ca680c040065
Sergi Chavez <schavezb@uoc.edu>
2022-03-08 20:57:51 +0100

    Corregidos errores ortograficos

--- src/app/canciones/modelos/datoscanciones.ts
+++ src/app/canciones/modelos/datoscanciones.ts
@@ -5,5 +5,5 @@ export const DatosCanciones:CancionModelo[]=[
 new CancionModelo("Kiss Of Fire", "Louis Armstrong", 1952, "Satchmo Signs", "Jazz","imagen2.jpeg"),
 new CancionModelo("Take Five", "The Dave Bruckek Quarter", 1959, "Time Out", "Jazz", "imagen3.jpeg"),
 new CancionModelo("Another One Bites The Dust", "Queen", 1980, "The Game", "Rock","imagen4.jpeg"),
-new CancionModelo("Come Toguether", "The Beatles", 1969, "Abbey Road", "Rock", "imagen1.jpeg")
+new CancionModelo("Come Together", "The Beatles", 1969, "Abbey Road", "Rock", "imagen1.jpeg")
 ]
