4863aed8d70dd847b84e1930712adb5a5aa9a4d4
rsanchezpaez <rsanchezpae@uoc.edu>
2022-04-14 10:27:29 +0200

    separate css code from view and clean code

--- src/app/canciones/listacancion/listacancion.component.ts
+++ src/app/canciones/listacancion/listacancion.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit,Input } from '@angular/core';
 import { CancionModelo } from '../modelos/cancion.modelo';
 import { DatosCanciones } from '../modelos/datoscanciones';
-import { HeaderComponent } from 'src/app/Header/header.component';
+
 
 @Component({
   selector: 'app-listacancion',
@@ -14,22 +14,17 @@ export class ListacancionComponent{
   public canciones=DatosCanciones;
   public cancion_selecionada?:CancionModelo;
   public cancion_reproductor?:CancionModelo;
-  public favorito:boolean=false;
-  constructor() {    
-  }
 
   ngOnInit(): void {
     this.searchField = 'autor'; 
   }
   
-
   ngDocheck(cancion: CancionModelo){
    this.cancion_selecionada=cancion; 
   }
+
   ngDoplay(cancion: CancionModelo){
     this.cancion_reproductor=cancion; 
    }
-  megusta(){
-    this.favorito=true;
-  }
+  
 }
